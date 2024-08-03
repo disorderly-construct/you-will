88d1eafa55107824418239488c97702d4cc56a55
disorderly-construct <117865654+disorderly-construct@users.noreply.github.com>
2024-08-03 14:24:07 -0400

    Tutorial 1.0

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 # main.py
+import sys
 from typewriter import typewriter_effect
 from input_handler import get_player_choice
-from tutorial import tutorial
+from tutorial import tutorial1
+from tutorial import tutorial2
 
 def main():
     # Get player name
     player_name = input("What is your name? ")
-    typewriter_effect(f"Welcome to my game, {player_name}!")
+    typewriter_effect(f"Welcome to the game, {player_name}.")
     
     # Ask if the player wants to go through the tutorial
     yn_tutorial = input("Would you like to go through the tutorial? (y/n) ").strip().lower()
     if yn_tutorial == "y":
-        tutorial()
+        tutorial1()
+        tutorial2()
     elif yn_tutorial == "n":
         pass
     elif yn_tutorial not in ["y", "n"]:
         typewriter_effect("Aren't you fuckin' special, eh?")
-        # exit() doesnt work yet
+        sys.exit("Goodbye")
     
     # Main game loop
-    typewriter_effect("You find yourself in a dark forest. What will you do?")
+    typewriter_effect("You find yourself in a dark, cold stone cell. The exit door is hardly attached. You've gotta be committed. You're in a dungeon, after all. Unsure how you got here, but you feel a sense of purpose.\n")
+    typewriter_effect("You see a heavy, marred wodden door in front of you. You move through.\n")
     
     while True:
         typewriter_effect("Choices: [explore, rest, quit]")
--- tutorial.py
+++ tutorial.py
@@ -1,8 +1,19 @@
 # tutorial.py
+import sys
 from typewriter import typewriter_effect
 
-def tutorial():
+def tutorial1():
     typewriter_effect("You will take part in a predominantly text-based game.")
     typewriter_effect("You will be given a scenario and options to choose from.")
-    typewriter_effect("You will type in the field your selection and enter continue.\n")
+    typewriter_effect("You will type in the field your selection and press enter continue.\n")
     typewriter_effect("You will be given a scenario with specific target values. You will not overshoot these values.\n")
+    
+def tutorial2():
+    tutorial2continue = input("Continue? (y/n)")
+    if tutorial2continue == "y":
+        typewriter_effect("Bring the subject to the required levels without going over. Deviate by 5 or less to cycle. Deviate by 1 or less to succeed.\n")
+        typewriter_effect("\nYou will not break the cycle.")
+        typewriter_effect("You will keep them alive. You will break them.")
+        typewriter_effect("You will.\n")
+    else:
+        sys.exit("Goodbye")
